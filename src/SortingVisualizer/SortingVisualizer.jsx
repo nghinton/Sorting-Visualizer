2e3b8d25bd9645d38f215344b94ecfc996b7b6ea
nghinton <nghinton@crimson.ua.edu>
2020-07-17 15:29:44 -0500

    Initial Commit

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import $ from 'jquery';
+import {getMergeSortAnimations} from '../sortingAlgorithms/sortingAlgorithms.js';
+import './SortingVisualizer.css';
+
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1.5;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = 'white';
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = 'red';
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = (($(window).innerWidth()-20) / 3) * 0.5;
+
+// Change this value for the height (height) of the bars in the array
+const HEIGHT_OF_ARRAY_BARS = $(window).innerHeight() * 0.8;
+
+
+
+export default class SortingVisualizer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      array: [],
+    };
+  }
+
+  componentDidMount() {
+    this.resetArray();
+  }
+
+  resetArray() {
+    const array = [];
+    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+      var value = Math.floor((HEIGHT_OF_ARRAY_BARS - 5) / NUMBER_OF_ARRAY_BARS) * i + 5;
+      array.push(value);
+    }
+    this.setState({array});
+  }
+
+  shuffleArray() {
+    const array = this.state.array;
+    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; ++i) {
+      var x = randomIntFromInterval(0, NUMBER_OF_ARRAY_BARS-1);
+      var y = randomIntFromInterval(0, NUMBER_OF_ARRAY_BARS-1);
+      this.swapValues(array, x, y)
+    }
+    this.setState({array});
+  }
+
+  swapValues(array, x, y) {
+    let temp = array[x];
+    array[x] = array[y];
+    array[y] = temp;
+  }
+
+  mergeSort() {
+    const animations = getMergeSortAnimations(this.state.array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName('array-bar');
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * ANIMATION_SPEED_MS);
+      }
+    }
+  }
+
+  quickSort() {
+    // We leave it as an exercise to the viewer of this code to implement this method.
+  }
+
+  heapSort() {
+    // We leave it as an exercise to the viewer of this code to implement this method.
+  }
+
+  bubbleSort() {
+    // We leave it as an exercise to the viewer of this code to implement this method.
+  }
+
+  render() {
+    const {array} = this.state;
+
+    return (
+      <div className="app-container">
+        <div className="array-container">
+          {array.map((value, idx) => (
+            <div
+              className="array-bar"
+              key={idx}
+              style={{
+                backgroundColor: PRIMARY_COLOR,
+                height: `${value}px`,
+              }}></div>
+          ))}
+        </div>
+        <div className="button-container">
+          <button onClick={() => this.shuffleArray()}>Shuffle Array</button>
+          <button onClick={() => this.mergeSort()}>Merge Sort</button>
+          <button onClick={() => this.quickSort()}>Quick Sort</button>
+          <button onClick={() => this.heapSort()}>Heap Sort</button>
+          <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+        </div>
+      </div>
+    );
+
+  }
+}
+
+// From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
